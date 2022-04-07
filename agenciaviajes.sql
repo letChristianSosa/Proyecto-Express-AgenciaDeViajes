5cfbb56562d638acff5d5e04ba45c91148694702
Christian Sosa <chrissm28@outlook.com>
2022-04-07 18:58:20 -0500

    Solucionado error en package.json

--- agenciaviajes.sql
+++ agenciaviajes.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.23, for macos10.13 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: agenciaviajes
 -- ------------------------------------------------------
--- Server version	5.7.31
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,24 +16,50 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `testimoniales`
+--
+
+DROP TABLE IF EXISTS `testimoniales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `testimoniales` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `correo` varchar(60) DEFAULT NULL,
+  `mensaje` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `testimoniales`
+--
+
+LOCK TABLES `testimoniales` WRITE;
+/*!40000 ALTER TABLE `testimoniales` DISABLE KEYS */;
+INSERT INTO `testimoniales` VALUES (1,'Chris','Chris@outlook.com','Ejemplo de testimonial');
+/*!40000 ALTER TABLE `testimoniales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `viajes`
 --
 
 DROP TABLE IF EXISTS `viajes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `viajes` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL,
   `titulo` varchar(60) DEFAULT NULL,
   `precio` varchar(10) DEFAULT NULL,
   `fecha_ida` date DEFAULT NULL,
   `fecha_vuelta` date DEFAULT NULL,
   `imagen` varchar(15) DEFAULT NULL,
   `descripcion` text,
-  `disponibles` int(11) DEFAULT NULL,
+  `disponibles` int DEFAULT NULL,
   `slug` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-05 11:21:33
+-- Dump completed on 2022-04-07 18:39:53
